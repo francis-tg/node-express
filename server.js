ffb99992736e8ed5e799caabb544448aef6c1acd
francis-tg <franc9885@gmail.com>
2021-07-29 21:20:49 +0200

    First commit

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express")
 const exprhbs = require("express-handlebars")
+const hbs = require("express-handlebars").create()
 
 const app = express()
 
 app.engine("handlebars", exprhbs())
 app.set("view engine", "handlebars")
-
+app.use(express.static("public"))
+hbs.getPartials().then(function (partials) {
+    console.log(partials);
+    // => { 'foo/bar': [Function],
+    // =>    title: [Function] }
+});
 app.get("/", (req, res)=>{
     res.render("home")
 })
 
 
 app.listen("5000",()=>{
-    console.log("Server running on port 6000")
+    console.log("Server running on port 5000")
 })
